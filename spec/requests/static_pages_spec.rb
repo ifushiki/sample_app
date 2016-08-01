9833c2425ecb66a67c41ce3b8917a52319087778
ifushiki <ifushiki@yahoo.com>
2016-08-01 11:09:15 -0700

    Modified static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_index_path
-      expect(response).to have_http_status(200)
+    
+    describe "Home page" do
+        it "should have the content 'Sample App'" do
+            get '/static_pages/home'
+            page.should have_content('Sample App')
+        end
     end
-  end
+    #  describe "GET /static_pages" do
+    #    it "works! (now write some real specs)" do
+    #  get static_pages_index_path
+    #  expect(response).to have_http_status(200)
+    #end
+    #end
 end
